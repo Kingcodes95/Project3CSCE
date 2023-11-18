534ce07917171d3203fc7c45ca1e937387ddc031
Ronnie Perez <ronnieperez@Ronnies-Air.lan>
2023-11-18 09:33:35 -0600

    Added functions mainframe and interface, functions do not work yet

--- Main.cpp
+++ Main.cpp
@@ -3,45 +3,74 @@ using namespace std;
 
 int number_of_students = 5;
 enum choices {Add = 1, Remove = 2, Display = 3, Search = 4, Results = 5, Quit = 6};
-choices choice;
+int choice = Results;
 
 struct Student {
     string nameStudent;
-    int studentID, testTaken;
+    int student_ID, tests_Taken;
     //pointer for dynamic array RIGHT HERE
     int avgScores;
 };
 
+void add_Student() {
+    cout << "Add (Function not complete)" << endl;
+}
+
+void remove_Student() {
+    cout << "Remove (Function not complete)" << endl;
+}
+
+void display_Data() {
+    cout << "Display (Function not complete)" << endl;
+} 
+
+void search_Data() {
+    cout << "Search (Function not complete)" << endl;
+}
+
+void export_Results() {
+    cout << "Results (Function not complete)" << endl;
+}
+
 int main()
 {
+    cout << "1. Add" << endl;
+    cout << "2. Remove" << endl;
+    cout << "3. Display" << endl;
+    cout << "4. Search" << endl;
+    cout << "5. Results" << endl;
+    cout << "6. Quit" << endl;
+    cout << "Please pick an option: ";
+    cin >> choice;
+
     switch (choice) {
         case Add:
-        //code
-        break;
+            add_Student();
+            break;
 
         case Remove:
-        //code
-        break;
+            remove_Student();
+            break;
 
         case Display:
-        //code
-        break;
+            display_Data();
+            break;
 
         case Search:
-        //code
-        break;
+            search_Data();
+            break;
 
         case Results:
-        //code
-        break;
+            export_Results();
+            break;
 
         case Quit:
-        //code
-        break;
+            cout << "Quit (Function not complete)" << endl;
+            break;
 
         default:
-        //code
-        break;
+            cout << "PLease enter a number 1 - 6" << endl;
+            break;
 
     }
     return 0;
--- README.md
+++ README.md
@@ -8,7 +8,11 @@ Started Project, but just did basic Github testing to understand things! I will 
 start the project during ThanksGiving Break!  
   
 **November 18th 1:41 AM:**
-Added enum for the menu and also created the student struct
+Added enum for the menu and also created the student struct  
+  
+  **November 18th 9:32 AM:**
+  Added all the functions(Functions aren't completed just the mainframe is added),  
+   and started slowly building the interface!
 
 
 
